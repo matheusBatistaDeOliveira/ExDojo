d054dc27ca5962bc609803eb20eacb040e0c94c7
Matheuszin <matheussetacursos@gmail.com>
2026-03-17 17:39:10 -0300

    tops

--- DevDojoGeral1/src/main/java/com/site/magnetic/maratonajava/intermediario/dominio/Estudante.java
+++ DevDojoGeral1/src/main/java/com/site/magnetic/maratonajava/intermediario/dominio/Estudante.java
@@ -1,7 +1,7 @@
 package com.site.magnetic.maratonajava.intermediario.dominio;
 
 public class Estudante {
-    int idade;
-    char sexo;
-    String nome;
+    public int idade;
+    public char sexo;
+    public String nome;
 }
--- DevDojoGeral1/src/main/java/com/site/magnetic/maratonajava/intermediario/test/PessoaTest01.java
+++ DevDojoGeral1/src/main/java/com/site/magnetic/maratonajava/intermediario/test/PessoaTest01.java
@@ -5,6 +5,17 @@ import com.site.magnetic.maratonajava.intermediario.dominio.Estudante;
 public class PessoaTest01 {
     public static void main(String[] args) {
 
-        Estudante nome; new Estudante[19,"M","Matheus"];//CONTINUE
+        Estudante estudante = new Estudante();//CONTINUE
+
+        estudante.nome = "Matheus";
+        estudante.sexo = 'M';
+        estudante.idade = 21;
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+        Estudante estudanteX = estudante;
+
+        System.out.println(estudanteX);
     }
 }
