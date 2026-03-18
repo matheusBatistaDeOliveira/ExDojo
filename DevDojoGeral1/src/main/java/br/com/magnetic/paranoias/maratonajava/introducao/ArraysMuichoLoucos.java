afbfd6227f6934bcba81f9644be257bc005bd366
Matheuszin <matheussetacursos@gmail.com>
2026-03-18 16:36:28 -0300

    tops

--- DevDojoGeral1/src/main/java/br/com/magnetic/paranoias/maratonajava/introducao/ArraysMuichoLoucos.java
+++ DevDojoGeral1/src/main/java/br/com/magnetic/paranoias/maratonajava/introducao/ArraysMuichoLoucos.java
@@ -24,29 +24,29 @@ public class ArraysMuichoLoucos {
         compensa[1][1] = 10;
         compensa[1][2] = 10;
 
-        int []resp = new int[compensa.length];
-        resp [0] = compensa[0][0] + compensa[0][1] + compensa[0][2];
-        resp [1] = compensa[1][0] + compensa[1][1] + compensa[1][2];
+        int []respLinhas = new int[compensa.length];
+        respLinhas [0] = compensa[0][0] + compensa[0][1] + compensa[0][2];
+        respLinhas [1] = compensa[1][0] + compensa[1][1] + compensa[1][2];
 
 
         System.out.println("------------MANUAL------------");
         System.out.println("------------------------------");
-        System.out.printf("eixo Y, total das colunas: %d", resp[0]);
+        System.out.printf("eixo Y, total das colunas: %d", respLinhas[0]);
         System.out.println("");
-        System.out.printf("eixo X, total das linhas: %d", resp[1]);
+        System.out.printf("eixo X, total das linhas: %d", respLinhas[1]);
         System.out.println("");
         System.out.println("------------------------------");
 
         for(int i = 0;i < compensa.length; i++){
             for(int x = 0; x < compensa[i].length; x++){
-                resp[i] = compensa[i][x];
+                respLinhas[i] += compensa[i][x];
             }
         }
         System.out.println("--------FLUXO REPETICAO-------");
         System.out.println("------------------------------");
-        System.out.printf("TOTAL de linhas: %d", resp[0]);
-        System.out.println("");
-        System.out.printf("TOTAL de colunas %d", resp[1]);
+        System.out.printf("TOTAL de linhas: %d", respLinhas[0]);
+        System.out.println(""); //fiz na lógica tabela[indice][eixos/dimensões], está errado, era pra ser tabela[linhas][colunas]
+        System.out.printf("TOTAL de colunas: %d", respLinhas[1]);
         System.out.println("");
         System.out.println("------------------------------");
         System.out.println("");
@@ -59,5 +59,10 @@ public class ArraysMuichoLoucos {
 //                System.out.println(compensas2);
 //            }
 //        }
+
+
+//        arays devem ser lidos assim: tabela[linha][coluna]
+//        pois é assim que são os formatos de suas APIs do banco
+//        toda linha tem seu próprio array, indexado pela coluna
     }
 }
